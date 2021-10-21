b7320b04ff39a8ee0eb36b68e9ab9fb3d5ef1672
DeathStinger <87364785+DeathStinger@users.noreply.github.com>
2021-10-21 08:56:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+status ="";
+function preload() {
+
+}
+
+function setup() {
+   canvas=createCanvas(380,380);
+   canvas.center();
+   video = createCapture(VIDEO);
+   video.size(380,380);
+   video.hide();
+   objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+   document.getElementById("status").innerHTML ="Status : Detecting Objects";
+}
